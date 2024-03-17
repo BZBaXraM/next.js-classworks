9f3c244bf70448960b84d6461927b05942e9b860
Bahram <baxram1997007@gmail.com>
2024-03-18 00:34:01 +0400

    removed product layout from app-router

--- app-router-intro/src/app/products/page.tsx
+++ app-router-intro/src/app/products/page.tsx
@@ -1,23 +1,23 @@
 import Link from "next/link";
-import { products } from "../../../lib/data";
+import {products} from "../../../lib/data";
 
 const Products = () => {
-  return (
-    <div>
-      <h1>Products</h1>
-      <ul>
-        {products.map(({ id, name }) => (
-          <Link key={id} href={`/products/${id}`}>
-            <li>
-              <h3>
-                {id}: {name}
-              </h3>
-            </li>
-          </Link>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Products</h1>
+            <ul>
+                {products.map(({id, name}) => (
+                    <Link key={id} href={`/products/${id}`}>
+                        <li>
+                            <h3>
+                                {id}: {name}
+                            </h3>
+                        </li>
+                    </Link>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Products;
